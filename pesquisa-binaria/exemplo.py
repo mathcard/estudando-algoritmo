51af774f77743c7df7c5f5a97a5db882b54e89df
Matheus do Carmo Cardoso <matheus.cardoso@faegsenar.local.br>
2024-08-16 13:55:20 -0300

    Algoritmo 1

---
+++ pesquisa-binaria/exemplo.py
@@ -0,0 +1,23 @@
+def pesquisa_binaria(lista, item):
+    baixo = 0;
+    alto = len(lista) - 1
+    
+    while baixo <= alto:
+        meio = (baixo+alto)//2
+        chute = lista[meio]
+        
+        print('Baixo:', baixo, 'Alto:', alto, 'Meio:', meio, 'Chute:', chute, 'Item:', item)
+        if chute == item:
+            return meio
+        if chute > item:
+            alto = meio - 1
+        else: 
+            baixo = meio + 1
+    return None
+
+minha_lista = [1, 3, 5, 7, 9]
+print(pesquisa_binaria(minha_lista, 3))
+print(pesquisa_binaria(minha_lista, -1))
+
+
+
