e3a2ebc0dd1519f07b9077d1be6c2501ae0e09ed
Matheus do Carmo Cardoso <matheus.cardoso@faegsenar.local.br>
2024-08-22 14:36:42 -0300

    iniciando estudo quicksort2

--- quicksort/algoritmo_quicksort.py
+++ quicksort/algoritmo_quicksort.py
@@ -9,6 +9,6 @@ def quicksort(array):
         print("Array: {}; Menores: {};  Pivo: {}; Maiores: {};".format(array, menores, pivo, maiores))        
         return quicksort(menores) + [pivo] + quicksort(maiores)        
 
-print(quicksort([10, 5, 2, 3, 70, 1]))
+print(quicksort([10, 5, 2]))
 
 
